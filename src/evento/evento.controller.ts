85966ce3609d839a4d0bf13be57bf073a5d7703e
Gibson-Arbey <gibsonarbey@gmail.com>
2025-03-27 20:47:47 -0500

    se agrega autorizacion a los servicios de eventos

--- src/evento/evento.controller.ts
+++ src/evento/evento.controller.ts
@@ -2,32 +2,38 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { EventoService } from './evento.service';
 import { CreateEventoDto } from './dto/create-evento.dto';
 import { UpdateEventoDto } from './dto/update-evento.dto';
+import { Auth } from 'src/auth/decorators/auth.decorator';
 
 @Controller('evento')
 export class EventoController {
   constructor(private readonly eventoService: EventoService) {}
 
   @Post()
+  @Auth()
   create(@Body() createEventoDto: CreateEventoDto) {
     return this.eventoService.create(createEventoDto);
   }
 
   @Get()
+  @Auth()
   findAll() {
     return this.eventoService.findAll();
   }
 
   @Get(':id')
+  @Auth()
   findOne(@Param('id') id: string) {
     return this.eventoService.findOne(id);
   }
 
   @Patch(':id')
+  @Auth()
   update(@Param('id') id: string, @Body() updateEventoDto: UpdateEventoDto) {
     return this.eventoService.update(id, updateEventoDto);
   }
 
   @Delete(':id')
+  @Auth()
   remove(@Param('id') id: string) {
     return this.eventoService.remove(id);
   }
